07a0d93d7ca52be16fc8ea9b8f5a637dd7e3936f
Hadisehzamani <hadisehzmn@gmail.com>
2023-08-07 15:36:18 +0330

    edit position

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let nav = document.querySelector('.nav')
 let openBtn = document.querySelector('button')
 let deleteBtn = document.querySelector('.delete')
+let btn = document.querySelector('.btn')
 
 
 openBtn.addEventListener('click', function(){
     nav.classList.add('active')
+    btn.style.marginLeft = '200px'
 })
 
 deleteBtn.addEventListener('click', function(){
     nav.classList.remove('active')
+    btn.style.marginLeft = '0'
 })
